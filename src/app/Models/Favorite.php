0f8ed113dec1685ba7c6f6689bff8f3ecc96debd
ayaka <aya_yu7@yahoo.co.jp>
2025-06-18 23:15:22 +0900

    Favoriteモデル作成

--- src/app/Models/Favorite.php
+++ src/app/Models/Favorite.php
@@ -8,17 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['user_id', 'item_id'];
-
-    // User↔︎Favoriteの関係（多対1）
-    public function user() {
-        return $this->belongsTo(User::class);
-    }
-
-    // Item↔︎Favoriteの関係（多対1）
-    public function item()
-    {
-        return $this->belongsTo(Item::class);
-    }
 }
