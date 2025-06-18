c3871ec362c9ff5ad0f2b07be23c3dc23d332892
ayaka <aya_yu7@yahoo.co.jp>
2025-06-18 23:01:50 +0900

    Commentモデル作成

--- src/app/Models/Comment.php
+++ src/app/Models/Comment.php
@@ -8,17 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['user_id', 'item_id', 'comment'];
-    // user↔︎comment 多対1の関係
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    // items↔︎comments 多対1の関係
-    public function item()
-    {
-        return $this->belongsTo(Item::class);
-    }
 }
