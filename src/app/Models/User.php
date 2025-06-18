5d6b8793bad1a292391430dfa495881a0dce2b95
ayaka <aya_yu7@yahoo.co.jp>
2025-06-18 23:38:37 +0900

    Userモデル修正

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,8 +7,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Item;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'image',
+        'postal_code',
+        'address',
+        'building',
+    ];
 }
