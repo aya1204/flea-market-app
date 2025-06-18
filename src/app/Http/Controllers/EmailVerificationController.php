63b53d523d7c129dd5747b9448dbe6044ac74579
ayaka <aya_yu7@yahoo.co.jp>
2025-06-18 15:50:49 +0900

    メール認証処理：__invokeメソッド追加

--- src/app/Http/Controllers/EmailVerificationController.php
+++ src/app/Http/Controllers/EmailVerificationController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
 /**
  * メール認証のコントローラー
  */
@@ -13,5 +16,20 @@ class EmailVerificationController extends Controller
     public function index()
     {
         return view('auth.verify-email');
+    }
+
+    /**
+     * メール認証処理
+     */
+    public function __invoke(EmailVerificationRequest $request)
+    {
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect('/mypage/profile');
+        }
+        if ($request->user()->markEmailAsVerified()) {
+            event(new Verified($request->user()));
+        }
+
+        return redirect()->route('profile.edit')->with('success', 'メール認証が完了しました。プロフィールを設定してください。');
     }
 }
