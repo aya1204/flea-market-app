f4ff8e5ed5e0a4a72de8c35fde73a1620d00b145
ayaka <aya_yu7@yahoo.co.jp>
2025-06-18 16:36:25 +0900

    SellController作成と出品画面表示：indexメソッド追加

--- src/app/Http/Controllers/SellController.php
+++ src/app/Http/Controllers/SellController.php
@@ -2,18 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ExhibitionRequest;
-use Illuminate\Http\Request;
-
 class SellController extends Controller
 {
+    // 出品画面表示
     public function index()
     {
         return view('sell.sell'); // resources/views/sell/sell.blade.php を表示
-    }
-
-    public function create(ExhibitionRequest $request)
-    {
-        return redirect('/')->with('success', '商品を出品しました。');
     }
 }
