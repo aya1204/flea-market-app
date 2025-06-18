7c8fbf410abdb4f5c17ad9ccb6903b55a86f2f0a
ayaka <aya_yu7@yahoo.co.jp>
2025-06-18 23:38:18 +0900

    Userモデル作成

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,68 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Models\Item;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
-
-    protected $table = 'users';
-    // itemsテーブル(出品)と多対１の関係
-    public function itemsForSale()
-    {
-        return $this->hasMany(Item::class, 'seller_user_id');
-    }
-
-    // itemsテーブル(購入)と多対1の関係
-    public function purchases()
-    {
-        return $this->hasMany(Item::class, 'purchase_user_id');
-    }
-
-    // favoritesテーブルと多対多の関係
-    public function favorites()
-    {
-        return $this->belongsToMany(Item::class, 'favorites')->withTimestamps();
-    }
-
-    // commentsテーブルと1対多の関係
-    public function comments()
-    {
-        return $this->hasMany(Comment::class);
-    }
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'image',
-        'postal_code',
-        'address',
-        'building',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
