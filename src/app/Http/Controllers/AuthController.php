a759ec7ec3e60305ee3c9a6b709bc11234941717
ayaka <aya_yu7@yahoo.co.jp>
2025-06-18 15:24:50 +0900

    AuthController作成とindexメソッド追加

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -2,12 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\RegisterRequest;
-use App\Models\User;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use App\Models\Item;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -20,61 +14,4 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-
-    /**
-     * 登録画面表示
-     */
-    public function register()
-    {
-        return view('auth.register');
-    }
-
-    //ユーザー登録処理
-    public function create(RegisterRequest $request)
-    {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        Auth::login($user);
-
-        return redirect()->route('profile.mypage_profile'); //プロフィール設定画面へ
-
-        event(new Registered($user));//Fortifyのメール認証用イベント
-        Auth::login($user);//ログイン状態にする
-
-        return redirect()->route('verification.notice'); //メール認証画面にリダイレクト
-    }
-
-    /**
-     * ログイン処理
-     */
-    public function login(Request $request)
-    {
-        $credentials = $request->only('email', 'password');
-
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return redirect()->intended('/');
-        }
-
-        return back()->withErrors([
-            'email' => 'ログイン情報が登録されていません。',
-            'password' => 'ログイン情報が登録されていません。',
-        ]);
-    }
-
-
-    /**
-     * ログアウト処理
-     */
-    public function logout(Request $request)
-    {
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        return redirect('/');
-    }
 }
