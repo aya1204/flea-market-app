bd40f1baa974a7702652a1ba8a60e4dcf850cbfe
ayaka <aya_yu7@yahoo.co.jp>
2025-06-18 15:42:57 +0900

    ログアウト処理：logoutメソッド追加

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -58,4 +58,16 @@ class AuthController extends Controller
             'password' => 'ログイン情報が登録されていません。',
         ]);
     }
+
+    /**
+     * ログアウト処理
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
 }
