a3cc81b4db2efafe5f19f22cd8e47c1fa0961f1b
ayaka <aya_yu7@yahoo.co.jp>
2025-06-19 12:52:43 +0900

    conditionsテーブル作成

--- src/database/migrations/2025_05_13_221126_create_conditions_table.php
+++ src/database/migrations/2025_05_13_221126_create_conditions_table.php
@@ -2,8 +2,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class CreateConditionsTable extends Migration
 {
@@ -14,11 +12,6 @@ class CreateConditionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('conditions', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 255);
-            $table->timestamps();
-        });
     }
 
     /**
@@ -28,6 +21,5 @@ class CreateConditionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('conditions');
     }
 }
