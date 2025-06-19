a8e97650ca5b8e870a0bf6cc29992e045123bc54
ayaka <aya_yu7@yahoo.co.jp>
2025-06-19 12:57:17 +0900

    favoritesテーブル：カラム作成

--- src/database/migrations/2025_05_13_221302_create_favorites_table.php
+++ src/database/migrations/2025_05_13_221302_create_favorites_table.php
@@ -2,6 +2,8 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateFavoritesTable extends Migration
 {
@@ -12,6 +14,13 @@ class CreateFavoritesTable extends Migration
      */
     public function up()
     {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->OnDelete('cascade');
+            $table->foreignId('item_id')->constrained()->OnDelete('cascade');
+            $table->unique(['user_id', 'item_id']);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -21,5 +30,6 @@ class CreateFavoritesTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('favorites');
     }
 }
