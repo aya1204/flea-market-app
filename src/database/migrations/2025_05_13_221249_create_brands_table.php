1d9ca1dd02ce49c99e01d67946c4d95a56b7acd0
ayaka <aya_yu7@yahoo.co.jp>
2025-06-19 12:54:31 +0900

    brandsテーブル作成

--- src/database/migrations/2025_05_13_221249_create_brands_table.php
+++ src/database/migrations/2025_05_13_221249_create_brands_table.php
@@ -1,9 +1,7 @@
-<!-- ブランドテーブル -->
+<!-- brandsテーブル -->
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class CreateBrandsTable extends Migration
 {
@@ -14,11 +12,6 @@ class CreateBrandsTable extends Migration
      */
     public function up()
     {
-        Schema::create('brands', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 255);
-            $table->timestamps();
-        });
     }
 
     /**
@@ -28,6 +21,5 @@ class CreateBrandsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('brands');
     }
 }
