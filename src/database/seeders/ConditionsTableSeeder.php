4adff09848cdf8d59f0efdf6b85c802c8a37b266
ayaka <aya_yu7@yahoo.co.jp>
2025-06-19 13:06:47 +0900

    conditionsテーブルシーダーファイル：シーディング作成

--- src/database/seeders/ConditionsTableSeeder.php
+++ src/database/seeders/ConditionsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ConditionsTableSeeder extends Seeder
 {
@@ -13,5 +14,11 @@ class ConditionsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('conditions')->insert([
+            ['name' => '良好'],               // ID:1
+            ['name' => '目立った傷や汚れなし'], // ID:2
+            ['name' => 'やや傷や汚れあり'],    // ID:3
+            ['name' => '状態が悪い'],         // ID:4
+        ]);
     }
 }
