808dbfcabaed4e8f49f1718ab8850d1425237944
ayaka <aya_yu7@yahoo.co.jp>
2025-06-19 13:04:05 +0900

    categoryテーブルシーダーファイル作成

--- src/database/seeders/CategoriesTableSeeder.php
+++ src/database/seeders/CategoriesTableSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -14,28 +13,5 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        $categories = [
-            'ファッション',
-            '家電',
-            'インテリア',
-            'レディース',
-            'メンズ',
-            'コスメ',
-            '本',
-            'ゲーム',
-            'スポーツ',
-            'キッチン',
-            'ハンドメイド',
-            'アクセサリー',
-            'おもちゃ',
-            'ベビー・キッズ',
-        ];
-
-        foreach ($categories as $index => $name) {
-            DB::table('categories')->insert([
-                'id' => $index + 1,
-                'name' => $name,
-            ]);
-        }
     }
 }
