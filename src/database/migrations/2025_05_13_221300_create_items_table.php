2b90bd651f41310ab88dc3608726cbb88926ca7e
ayaka <aya_yu7@yahoo.co.jp>
2025-06-19 12:55:56 +0900

    itemsテーブル作成

--- src/database/migrations/2025_05_13_221300_create_items_table.php
+++ src/database/migrations/2025_05_13_221300_create_items_table.php
@@ -2,8 +2,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class CreateItemsTable extends Migration
 {
@@ -14,25 +12,6 @@ class CreateItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
-            $table->id();
-            // 出品者
-            $table->foreignId('seller_user_id')->constrained('users')->OnDelete('cascade');
-            // 購入者
-            $table->foreignId('purchase_user_id')->nullable()->constrained('users');
-            $table->foreignId('condition_id')->constrained('conditions')->OnDelete('cascade');
-            $table->foreignId('brand_id')->nullable()->constrained('brands')->OnDelete('cascade');
-            $table->foreignId('paymentmethod_id')->nullable()->constrained('paymentmethods')->OnDelete('cascade');
-            $table->string('image');
-            $table->string('title');
-            $table->text('description');
-            $table->integer('price');
-            $table->boolean('is_sold')->default(false);
-            $table->string('postal_code')->nullable(); // 購入時郵便番号
-            $table->string('address')->nullable();     // 購入時住所
-            $table->string('building')->nullable();    // 購入時建物名
-            $table->timestamps();
-        });
     }
 
     /**
@@ -42,6 +21,5 @@ class CreateItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('items');
     }
 }
