295f3654e98f7d2a7df83b622a3341793cc22d3e
ayaka <aya_yu7@yahoo.co.jp>
2025-06-19 13:13:17 +0900

    usersテーブルシーダーファイル作成

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,32 +13,5 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => '山田 太郎',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
-            'image' => 'user1_icon.png',
-            'postal_code' => '123-4567',
-            'address' => '東京都渋谷区千駄ヶ谷1-2-3',
-            'building' => ' 千駄ヶ谷マンション101',
-        ]);
-        User::create([
-            'name' => '佐藤 花子',
-            'email' => 'test2@example.com',
-            'password' => Hash::make('password'),
-            'image' => 'user2_icon.png',
-            'postal_code' => '123-4567',
-            'address' => '東京都渋谷区千駄ヶ谷1-2-3',
-            'building' => ' 千駄ヶ谷マンション102',
-        ]);
-        User::create([
-            'name' => '鈴木 一郎',
-            'email' => 'test3@example.com',
-            'password' => Hash::make('password'),
-            'image' => 'user3_icon.png',
-            'postal_code' => '123-4567',
-            'address' => '東京都渋谷区千駄ヶ谷1-2-3',
-            'building' => ' 千駄ヶ谷マンション103',
-        ]);
     }
 }
