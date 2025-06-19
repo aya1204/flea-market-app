b81c0102fa8529ca9bda4143774759a98a45614d
ayaka <aya_yu7@yahoo.co.jp>
2025-06-19 13:01:01 +0900

    commentsテーブル：カラム作成

--- src/database/migrations/2025_05_13_221428_create_comments_table.php
+++ src/database/migrations/2025_05_13_221428_create_comments_table.php
@@ -2,6 +2,8 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateCommentsTable extends Migration
 {
@@ -12,6 +14,13 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('item_id')->constrained('items')->onDelete('cascade');
+            $table->string('comment', 255)->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -21,5 +30,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('comments');
     }
 }
