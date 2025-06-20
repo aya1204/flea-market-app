206cc1b94c89d1e3ac34cf4d0d2f7122edb4b6ec
ayaka <aya_yu7@yahoo.co.jp>
2025-06-20 23:50:05 +0900

    ルーティング定義：会員登録処理ルートを追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\SellController;
-use App\Http\Controllers\PurchaseController;
-use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
-use App\Http\Controllers\EmailVerificationController;
 
 /*
 |--------------------------------------------------------------------------
