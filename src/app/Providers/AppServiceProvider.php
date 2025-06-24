659432d5c193ea52512347046593556e9f8b2d18
ayaka <aya_yu7@yahoo.co.jp>
2025-06-24 18:23:48 +0900

    AppServiceProviderコメントアウト追加

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -24,10 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // ログイン画面表示設定
         Fortify::loginView(function () {
             return view('auth.login');
         });
 
+        // 会員登録画面表示設定
         Fortify::registerView(function () {
             return view('auth.register');
         });
