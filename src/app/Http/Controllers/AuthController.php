0354034b6134ca03f10bff46b4134e331acbd981
ayaka <aya_yu7@yahoo.co.jp>
2025-06-24 19:51:44 +0900

    AuthController修正

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -54,7 +54,7 @@ class AuthController extends Controller
         ]);
 
         Auth::login($user);
-        return redirect()->route('profile.edit');
+            return redirect()->route('profile.edit');
     }
 
     /**
