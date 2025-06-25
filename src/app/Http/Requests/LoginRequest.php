6efeb68a431a84f3486758a5db643b7f45c7991c
ayaka <aya_yu7@yahoo.co.jp>
2025-06-25 20:02:06 +0900

    LoginRequestのバリデーションメッセージ定義を修正

--- src/app/Http/Requests/LoginRequest.php
+++ src/app/Http/Requests/LoginRequest.php
@@ -29,7 +29,7 @@ class LoginRequest extends FormRequest
         ];
     }
 
-    public function message()
+    public function messages()
     {
         return [
             'email.required' => 'メールアドレスを入力してください',
