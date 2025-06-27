09d4b0958920095eca6ea373908e81ab78ad6a99
ayaka <aya_yu7@yahoo.co.jp>
2025-06-28 08:24:57 +0900

    AuthControllerインデント修正

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -57,7 +57,7 @@ class AuthController extends Controller
         ]);
 
         Auth::login($user);
-            return redirect()->route('profile.edit');
+        return redirect()->route('profile.edit');
     }
 
     /**
