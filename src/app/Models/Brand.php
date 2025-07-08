57aa3ca31a163a06a4dbe8fcec2c54d04edd372d
ayaka <aya_yu7@yahoo.co.jp>
2025-07-08 19:29:08 +0900

    itemsテーブルとのリレーションを追加

--- src/app/Models/Brand.php
+++ src/app/Models/Brand.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     use HasFactory;
+    // itemsテーブルと1対多の関係
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
