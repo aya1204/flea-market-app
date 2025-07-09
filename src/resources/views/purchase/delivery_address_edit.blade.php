fb434c598c2de1ec05cfbfd7b7719496915dd605
ayaka <aya_yu7@yahoo.co.jp>
2025-07-10 00:40:57 +0900

    クラス名を修正

--- src/resources/views/purchase/delivery_address_edit.blade.php
+++ src/resources/views/purchase/delivery_address_edit.blade.php
@@ -9,18 +9,18 @@
 
 <div class="profile-container">
     <h2 class="delivery-address-edit-title">住所の変更</h2>
-    <div class="profile-detail_form">
+    <div class="profile-detail-form">
         <form method="POST" action="{{ route('purchase.update', ['item' => $item->id]) }}" enctype="multipart/form-data">
             @csrf
-            <div class="profile">
+            <div class="profile-form-group">
                 <label for="postal_code" class="postal_code">郵便番号</label>
                 <input id="postal_code" type="text" name="postal_code" value="{{ old('postal_code', $user->postal_code) }}" class="text-box">
             </div>
-            <div class="profile">
+            <div class="profile-form-group">
                 <label for="address" class="address">住所</label>
                 <input id="address" type="text" name="address" value="{{ old('address', $user->address) }}" class="text-box">
             </div>
-            <div class="profile">
+            <div class="profile-form-group">
                 <label for="building" class="building">建物名</label>
                 <input id="building" type="text" name="building" value="{{ old('building', $user->building) }}" class="text-box">
             </div>
