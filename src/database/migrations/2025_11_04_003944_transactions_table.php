35a61978ebef81a948700b5101c4b911ef863b83
ayaka <aya_yu7@yahoo.co.jp>
2025-11-05 23:24:08 +0900

    Transactionテーブルを作成

---
+++ src/database/migrations/2025_11_04_003944_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            // 出品者
+            $table->foreignId('seller_user_id')->constrained('users')->OnDelete('cascade');
+            // 購入者
+            $table->foreignId('purchase_user_id')->nullable()->constrained('users');
+            $table->foreignId('item_id')->constrained()->OnDelete('cascade');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
