72e7af30e98620e2a1d3559a490663c7227dc963
ayaka <aya_yu7@yahoo.co.jp>
2025-11-05 23:23:00 +0900

    TransactionReviewモデルを追加し、取引メッセージに関するリレーションと属性を定義

---
+++ src/app/Models/TransactionReview.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionReview extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'transaction_id',
+        'reviewer_id',
+        'rating',
+        'comment',
+    ];
+
+    // テーブル名の指定
+    protected $table = 'transaction_reviews';
+
+    // このメッセージが紐づく取引(transaction)と1対1の関係
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+
+    // レビューした人（出品者or購入者）と多対1の関係
+    public function reviewer()
+    {
+        return $this->belongsTo(User::class, 'reviewer_id');
+    }
+
+    // レビューされた人（出品者or購入者）と多対1の関係
+    public function reviewee()
+    {
+        return $this->belongsTo(User::class, 'reviewee_id');
+    }
+}
