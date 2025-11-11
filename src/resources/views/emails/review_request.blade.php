a2a94071b09db26ce285642239082ff741ab392c
ayaka <aya_yu7@yahoo.co.jp>
2025-11-11 22:35:23 +0900

    メール本文を表示するよう修正

--- src/resources/views/emails/review_request.blade.php
+++ src/resources/views/emails/review_request.blade.php
@@ -1,4 +1,12 @@
 <!-- 取引完了申請メールのBlade -->
-<p>{{ $transaction->purchase->name }} さんから評価が届きました。</p>
-<p>取引を完了してください。</p>
-<p><a href="{{ route('transaction.show', ['transaction' => $transaction->id]) }}">取引完了画面を開く</a></p>
+@component('mail::message')
+# 取引評価が届きました
+{{ $transaction->purchase->name}} さんから評価が届きました。
+
+取引チャット画面で取引を完了してください。
+@component('mail::button', ['url' => route('transaction.show', ['transaction' => $transaction->id])])
+取引チャット画面を開く
+@endcomponent
+
+よろしくお願いいたします。
+@endcomponent
